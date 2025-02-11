a6950a745a0e6f100cc7a01e617e5d21c83d7dfe
mozaffarsentientgeeks <sk.mozaffar@sentientgeeks.com>
2025-02-11 13:40:23 +0530

    Boolean search script added

--- src/test/java/fcaa/testCase/glossary.java
+++ src/test/java/fcaa/testCase/glossary.java
@@ -166,35 +166,4 @@ public class glossary extends BaseClass {
 		boolean isValid = glossary.validateResults(query);
 		Assert.assertTrue(isValid, "Search results do not match for query: " + query);
 	}
-=======
-import org.testng.annotations.Test;
-
-import fcaa.AbstractComponnent.MenuBar;
-import fcaa.pageObject.Glossary;
-import fcaa.testComponent.BaseClass;
-
-public class glossary extends BaseClass {
-
-	// Latest News Object
-		MenuBar menuBar;
-		Glossary glossary;
-
-		@BeforeMethod
-		public void setUpGlossary() {
-			menuBar = new MenuBar(driver);
-			glossary = menuBar.goToGlossary();
-		}
-		
-		// <--------------Test cases for the glossary Home page---------------->
-
-		// Verify Search functionality
-		@Test(priority = 1)
-		public void testsearchbox() throws InterruptedException {
-			String searchPlaceHolder = glossary.;
-			Assert.assertTrue(searchPlaceHolder.equalsIgnoreCase("Search Glossary"));
-		}
-
-		
-		
->>>>>>> branch 'master' of https://github.com/atanusenti/FCA.git
 }
